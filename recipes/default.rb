5f87f88efb1ec3edb04bab75783982c164c15756
poladinesh <dp.websphere@gmail.com>
2017-03-30 21:39:42 -0500

    added support for centos & ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,14 @@
 
-execute "apt-get update" do
-	command "apt-get update"
+if node['platform'] == "ubuntu"
+  execute "apt-get update" do	
+  	command "apt-get update -y"
+  end
+elsif node['platform'] == "centos"
+  execute "yum update" do
+  	command "yum update -y"
+  end
+else
+  log "nothing to update"
 end
 
 package "git"
